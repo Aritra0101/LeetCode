b7a0e40b40d43f77704473513cd18fa277ea5011
Aritra Chatterjee <97108200+Aritra0101@users.noreply.github.com>
2023-10-30 01:20:03 +0530

    Time: 0 ms (100.00%) | Memory: 39.1 MB (47.66%) - LeetSync

---
+++ 458-poor-pigs/poor-pigs.java
@@ -0,0 +1,26 @@
+// // class Solution {
+// //     public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        
+// //     }
+// // }
+
+// class Solution {
+//   public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+//     return (int) Math.ceil(Math.log(buckets) / Math.log(minutesToTest / minutesToDie + 1));
+//   }
+// }
+
+
+
+class Solution {
+    public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        // int result=(int)Math.ceil(( (int)(Math.log(buckets)/Math.log(2)))/((int)(Math.log((int)(minutesToTest/minutesToDie)+1)/Math.log(2))));
+        // return result;
+        int res=minutesToTest/minutesToDie,ans=0;
+        while((int)Math.pow(res+1,ans)<buckets)
+        {
+            ans++;
+        }
+        return ans;
+    }
+}

597f8adf0d2dc55cca15594732d803b75338049c
Aritra Chatterjee <97108200+Aritra0101@users.noreply.github.com>
2023-11-03 03:02:51 +0530

    Time: 143 ms (32.42%) | Memory: 60.5 MB (16.47%) - LeetSync

--- 1512-design-underground-system/design-underground-system.cpp
+++ 1512-design-underground-system/design-underground-system.cpp
@@ -1,10 +1,39 @@
+// class UndergroundSystem {
+// public:
+//     unordered_map<int, pair<string, int>> passenger;
+//     unordered_map<string, vector<int>> stationTime;
+//     UndergroundSystem() {
+//         unordered_map<int, pair<string, int>> passenger;
+//         unordered_map<string, vector<int>> stationTime;
+//     }
+    
+//     void checkIn(int id, string stationName, int t) {
+//         passenger[id] = make_pair(stationName, t);
+//     }
+    
+//     void checkOut(int id, string stationName, int t) {
+//         stationTime[ passenger[id].first + '->' + stationName ].emplace_back( abs(passenger[id].second - t) ); 
+//         passenger.erase(id);
+//     }
+    
+//     double getAverageTime(string startStation, string endStation) {
+//         double avg = 0;
+
+//         for(int time: stationTime[startStation + '->' + endStation]) {
+//             avg += time;
+//         }
+
+//         return avg/stationTime[startStation + '->' + endStation].size();
+//     }
+// };
+
 class UndergroundSystem {
 public:
     unordered_map<int, pair<string, int>> passenger;
-    unordered_map<string, vector<int>> stationTime;
+    unordered_map<string, pair<int, int>> stationTime;
     UndergroundSystem() {
         unordered_map<int, pair<string, int>> passenger;
-        unordered_map<string, vector<int>> stationTime;
+        unordered_map<string, pair<int, int>> stationTime;
     }
     
     void checkIn(int id, string stationName, int t) {
@@ -12,18 +41,13 @@ public:
     }
     
     void checkOut(int id, string stationName, int t) {
-        stationTime[ passenger[id].first + '-' + stationName ].emplace_back( abs(passenger[id].second - t) ); 
+        stationTime[ passenger[id].first + "->" + stationName ].first += abs(passenger[id].second - t);
+        stationTime[ passenger[id].first + "->" + stationName ].second += 1; 
         passenger.erase(id);
     }
     
     double getAverageTime(string startStation, string endStation) {
-        double avg = 0;
-
-        for(int time: stationTime[startStation + '-' + endStation]) {
-            avg += time;
-        }
-
-        return avg/stationTime[startStation + '-' + endStation].size();
+        return double(stationTime[startStation + "->" + endStation].first)/double(stationTime[startStation + "->" + endStation].second);
     }
 };
 

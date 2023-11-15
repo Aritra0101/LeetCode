769d5bfa0ba47eb21e6d234f530b8d5f7b99d58d
Aritra Chatterjee <97108200+Aritra0101@users.noreply.github.com>
2023-11-16 01:33:50 +0530

    Time: 4 ms (31.19%) | Memory: 6.5 MB (18.03%) - LeetSync

--- 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.cpp
+++ 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
+    // int differenceOfSums(int n, int m) {
+    //     long num1 = 0;
+    //     long num2 = 0;
+
+    //     for(int i=1; i<=n; i++) {
+    //         if(i % m == 0 ) {
+    //             num2 += i;
+    //         } else {
+    //             num1 += i;
+    //         }
+    //     }
+
+    //     return num1-num2;
+    // }
+
     int differenceOfSums(int n, int m) {
-        long num1 = 0;
+        long num1 = (n * (n+1))/2;
         long num2 = 0;
-
-        for(int i=1; i<=n; i++) {
-            if(i % m == 0 ) {
-                num2 += i;
-            } else {
-                num1 += i;
-            }
+        for(int i=m; i<=n; i+=m) {
+            num2 += i;
         }
-
+        num1 -= num2;
         return num1-num2;
     }
 };

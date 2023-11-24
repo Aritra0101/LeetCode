8a4ccc495d9699fa4632d296095bb404f3e105fd
Aritra Chatterjee <97108200+Aritra0101@users.noreply.github.com>
2023-11-24 17:53:33 +0530

    Time: 511 ms (86.77%) | Memory: 28.7 MB (21.94%) - LeetSync

---
+++ 1683-maximum-number-of-coins-you-can-get/maximum-number-of-coins-you-can-get.py
@@ -0,0 +1,7 @@
+# class Solution:
+#     def maxCoins(self, piles: List[int]) -> int:
+        
+
+class Solution:
+  def maxCoins(self, piles: List[int]) -> int:
+    return sum(sorted(piles)[len(piles) // 3::2])

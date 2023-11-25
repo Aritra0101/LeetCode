c510ea38adce76f0b20da80d00f9abe58070a037
Aritra Chatterjee <97108200+Aritra0101@users.noreply.github.com>
2023-11-26 01:06:23 +0530

    Time: 114 ms (90.16%) | Memory: 65.9 MB (91.69%) - LeetSync

--- 1539-diagonal-traverse-ii/diagonal-traverse-ii.cpp
+++ 1539-diagonal-traverse-ii/diagonal-traverse-ii.cpp
@@ -19,25 +19,22 @@ public:
     // }
 
     vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
-        set<pair<int, int>> mySet;
         vector<int> res;
         int n = nums.size();
         queue<pair<int,int>> qu;
         qu.push( {0,0} );
-        mySet.insert( {0,0} );
         pair<int, int> p;
 
         while(!qu.empty()) {
             p = qu.front();
             qu.pop();
-            cout<<p.first<<" "<<p.second<<endl;
-            mySet.insert(p);
+
             res.emplace_back(nums[p.first][p.second]);
 
-            if(p.second==0 && p.first+1 < n && mySet.find({p.first+1, p.second})==mySet.end()) {
+            if(p.second==0 && p.first+1 < n) {
                 qu.push( {p.first+1, p.second} );
             }
-            if(p.second+1 < nums[p.first].size() && mySet.find({p.first, p.second+1})==mySet.end()) {
+            if(p.second+1 < nums[p.first].size()) {
                 qu.push( {p.first, p.second+1} );
             }
         }

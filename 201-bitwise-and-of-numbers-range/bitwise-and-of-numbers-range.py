f988cb4576dac5a2685da6e6d3713345d2fe1f3c
Aritra Chatterjee <97108200+Aritra0101@users.noreply.github.com>
2024-02-22 00:07:06 +0530

    Time: 56 ms (42.90%) | Memory: 16.6 MB (81.69%) - LeetSync

---
+++ 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.py
@@ -0,0 +1,7 @@
+# class Solution:
+#     def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        
+
+class Solution:
+  def rangeBitwiseAnd(self, m: int, n: int) -> int:
+    return self.rangeBitwiseAnd(m >> 1, n >> 1) << 1 if m < n else m

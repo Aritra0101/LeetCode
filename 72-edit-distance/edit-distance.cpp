b410a50c8c74d29812a684139c4f25eb0f8ddbb3
Aritra Chatterjee <97108200+Aritra0101@users.noreply.github.com>
2024-03-14 01:54:05 +0530

    Time: 13 ms (30.40%) | Memory: 11.9 MB (51.15%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,23 +1,23 @@
 class Solution {
 private:
-    // int solve01(string word1, string word2, int n1, int n2) {
-    //     if(n1 < 0) {
-    //         return n2 + 1;
-    //     }
-    //     if(n2 < 0) {
-    //         return n1 + 1;
-    //     }
+    int solve01(string word1, string word2, int n1, int n2) {
+        if(n1 < 0) {
+            return n2 + 1;
+        }
+        if(n2 < 0) {
+            return n1 + 1;
+        }
 
-    //     if(word1[n1] == word2[n2]) {
-    //         return solve01(word1, word2, n1-1, n2-1);
-    //     }
+        if(word1[n1] == word2[n2]) {
+            return solve01(word1, word2, n1-1, n2-1);
+        }
 
-    //     int c1 = solve01(word1, word2, n1, n2-1) + 1; //insert
-    //     int c2 = solve01(word1, word2, n1-1, n2) + 1; //delete
-    //     int c3 = solve01(word1, word2, n1-1, n2-1) + 1; //replace
+        int c1 = solve01(word1, word2, n1, n2-1) + 1; //insert
+        int c2 = solve01(word1, word2, n1-1, n2) + 1; //delete
+        int c3 = solve01(word1, word2, n1-1, n2-1) + 1; //replace
 
-    //     return min(c1, min(c2, c3));
-    // }
+        return min(c1, min(c2, c3));
+    }
 
     int solve02(string word1, string word2, int n1, int n2, vector<vector<int>>& dp) {
         if(n1 < 0) {
@@ -49,7 +49,26 @@ public:
 
         // return solve01(word1, word2, n1-1, n2-1);
 
-        vector<vector<int>> dp(n1+1, vector<int>(n2+1, -1));
-        return solve02(word1, word2, n1-1, n2-1, dp);
+        // vector<vector<int>> dp(n1+1, vector<int>(n2+1, -1));
+        // return solve02(word1, word2, n1-1, n2-1, dp);
+
+        vector<vector<int>> dp(n1+1, vector<int>(n2+1, 0));
+        for(int i=0; i<=n1; i++) {
+            dp[i][0] = i;
+        }
+        for(int j=0; j<=n2; j++) {
+            dp[0][j] = j;
+        }
+        dp[0][0] = 0;
+        for(int i=1; i<=n1; i++) {
+            for(int j=1; j<=n2; j++) {
+                if(word1[i-1] == word2[j-1]) {
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    dp[i][j] = min(dp[i-1][j-1], min(dp[i-1][j], dp[i][j-1])) + 1;
+                }
+            }
+        }
+        return dp[n1][n2];
     }
 };
